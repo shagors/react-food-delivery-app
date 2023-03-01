680b71c0796c4d583044f4f2ff90025a101dda3e
shajjad Shagor <shajjadshagor@gmail.com>
2023-03-01 23:28:19 +0600

    design category menu and some other sections add

---
+++ src/components/UI/category/Category.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import categoryImg01 from "../../../assets/images/category-01.png";
+import categoryImg02 from "../../../assets/images/category-02.png";
+import categoryImg03 from "../../../assets/images/category-03.png";
+import categoryImg04 from "../../../assets/images/category-04.png";
+
+import "../../../styles/category.css";
+
+const category = [
+  {
+    display: "Fast Food",
+    imgUrl: categoryImg01,
+  },
+  {
+    display: "Pizza",
+    imgUrl: categoryImg02,
+  },
+  {
+    display: "Asian Food",
+    imgUrl: categoryImg03,
+  },
+  {
+    display: "Raw Meat",
+    imgUrl: categoryImg04,
+  },
+];
+
+const Category = () => {
+  return (
+    <Container>
+      <Row>
+        {category.map((item, index) => (
+          <Col lg="3" md="4" className="">
+            <div className="category__item d-flex align-items-center gap-3">
+              <div className="category__img">
+                <img src={item.imgUrl} alt="Category Img" />
+              </div>
+              <h6>{item.display}</h6>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Category;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,29 @@ import Helmet from "../components/Helmet/Helmet";
 import heroImg from "../assets/images/hero.png";
 import "../styles/hero-section.css";
 import { Link } from "react-router-dom";
+import Category from "../components/UI/category/Category";
+import "../styles/home.css";
+import featureImg01 from "../assets/images/service-01.png";
+import featureImg02 from "../assets/images/service-02.png";
+import featureImg03 from "../assets/images/service-03.png";
+
+const featureData = [
+  {
+    title: "Quick Delivery",
+    imgUrl: featureImg01,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, sit nobis nisi sunt consequatur similique voluptatem.",
+  },
+  {
+    title: "Super Dine In",
+    imgUrl: featureImg02,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, sit nobis nisi sunt consequatur similique voluptatem.",
+  },
+  {
+    title: "Easy Pick Up",
+    imgUrl: featureImg03,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, sit nobis nisi sunt consequatur similique voluptatem.",
+  },
+];
 
 const Home = () => {
   return (
@@ -57,6 +80,46 @@ const Home = () => {
                 <img src={heroImg} alt="hero-img" className="w-100" />
               </div>
             </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="pt-0">
+        <Category />
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h5 className="feature__subtitle mb-4">What we serve</h5>
+              <h2 className="feature__title">Just sit back at home</h2>
+              <h2 className="feature__title">
+                we will <span>take care</span>
+              </h2>
+              <p className="mb-1 mt-4 feature__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                porro deserunt qui cumque voluptate sit deleniti
+              </p>
+              <p className="feature__text">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Minima, odit!
+              </p>
+            </Col>
+
+            {featureData.map((item, index) => (
+              <Col lg="4" md="4" key={index} className="mt-5">
+                <div className="feature__item text-center px-5 py-3">
+                  <img
+                    src={item.imgUrl}
+                    alt="feature-img"
+                    className="w-25 mb-3"
+                  />
+                  <h5 className="fw-bold mb-3">{item.title}</h5>
+                  <p className="">{item.desc}</p>
+                </div>
+              </Col>
+            ))}
           </Row>
         </Container>
       </section>
