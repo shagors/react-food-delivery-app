87f26597eb8df4619a92accc7a8c536639434f2b
shajjad Shagor <shajjadshagor@gmail.com>
2023-03-05 12:39:21 +0600

    paginate use for pagination and its check.. work properly

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -4,11 +4,38 @@ import CommonSection from "../components/UI/common-section/CommonSection";
 import { Col, Container, Row } from "reactstrap";
 import products from "../assets/fake-data/products";
 import ProductCard from "../components/UI/product-card/ProductCard";
+import ReactPaginate from "react-paginate";
+
 import "../styles/all-foods.css";
+import "../styles/pagination.css";
 
 const AllFoods = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [productData, setProductData] = useState(products);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const searchedProduct = products.filter((item) => {
+    if (searchTerm.value === "") {
+      return item;
+    }
+    if (item.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+      return item;
+    } else {
+      return console.log("not found");
+    }
+  });
+
+  const productPerPage = 12;
+  const visitedPage = pageNumber * productPerPage;
+  const displayPage = searchedProduct.slice(
+    visitedPage,
+    visitedPage + productPerPage
+  );
+
+  const pageCount = Math.ceil(searchedProduct.length / productPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   return (
     <Helmet title="All-Foods">
@@ -42,7 +69,7 @@ const AllFoods = () => {
               </div>
             </Col>
 
-            {productData
+            {displayPage
               .filter((item) => {
                 if (searchTerm.value === "") return item;
                 if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
@@ -54,6 +81,16 @@ const AllFoods = () => {
                   <ProductCard item={item} />
                 </Col>
               ))}
+
+            <div>
+              <ReactPaginate
+                pageCount={pageCount}
+                onPageChange={changePage}
+                previousLabel={"Prev"}
+                nextLabel={"Next"}
+                containerClassName=" paginationBttns "
+              />
+            </div>
           </Row>
         </Container>
       </section>
